36b717516b8f090adfb886c3ffc8f7d1ea74cc1d
Daniel Grieve <dnlgrv@hey.com>
2022-08-23 12:27:17 +0100

    Update airports and add JSON endpoint

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -1,16 +1,24 @@
 class AirportsController < ApplicationController
   AIRPORTS = [
-    Airport.new("EGNT", "Newcastle"),
-    Airport.new("EGLL", "Heathrow"),
-    Airport.new("EGPH", "Edinburgh"),
-    Airport.new("KBWI", "Baltimore"),
-    Airport.new("KJFK", "JFK"),
-    Airport.new("CYOW", "Ontario"),
-    Airport.new("MMIA", "Colima")
+    Airport.new("CYOW", "Ottawa", 45.3192, -75.6692),
+    Airport.new("EGNT", "Newcastle", 55.0393, -1.6931),
+    Airport.new("EGLL", "Heathrow", 51.4700, -0.4543),
+    Airport.new("EGPH", "Edinburgh", 55.9508, -3.3615),
+    Airport.new("KBWI", "Baltimore", 39.1774, -76.6684),
+    Airport.new("KJFK", "JFK", 40.6413, -73.7781),
+    Airport.new("MMIA", "Colima", 19.2806, -103.5774)
   ]
 
   def index
     render "autocomplete/list", locals: {dom_id: params[:dom_id], entries: airports_matching(params[:q]), value: :icao, name: :display_name}
+  end
+
+  def show
+    respond_to do |format|
+      format.json do
+        render json: find_airport(params[:id])
+      end
+    end
   end
 
   private
@@ -21,5 +29,9 @@ class AirportsController < ApplicationController
     AIRPORTS.filter do |airport|
       airport.name =~ /#{query}/i || airport.icao =~ /^#{query}/i
     end
+  end
+
+  def find_airport(icao)
+    AIRPORTS.find { |airport| airport.icao.downcase == icao.downcase }
   end
 end
